d66a52e1fbcde3248c534e4f8180a167e1b73d2f
abcdapp <ashoknsb@gmail.com>
2022-02-08 17:08:48 -0800

    added inheritance

--- first.py
+++ first.py
@@ -10,12 +10,24 @@ class Person:
         print(self.age)
 
 
-person1 = Person("xyz", 10)
-person2 = Person("abc", 11)
+# person1 = Person("xyz", 10)
+# person2 = Person("abc", 11)
 
-person1.personDetails()
-person2.personDetails()
+# person1.personDetails()
+# person2.personDetails()
+
+class Parent(Person):
+    def __init__(self, name, age, job):
+        super().__init__(name,age)
+        self.job = job
+    
+    def parentDetails(self):
+        print(self.name)
+        print(self.age)
+        print(self.job)
 
 
+parent1 = Parent("ashok,", 12, "swimmer")
+parent1.parentDetails()
 
 
